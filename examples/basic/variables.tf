dac670608e1c5e5d0958319f83fbdfc355923244
Gus Narea <gnarea@users.noreply.github.com>
2023-07-19 23:09:46 +0100

    format

--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -1,9 +1,9 @@
 variable "mongodbatlas_public_key" {
-    description = "MongoDB Atlas public key"
+  description = "MongoDB Atlas public key"
 }
 
 variable "mongodbatlas_private_key" {
-    description = "MongoDB Atlas private key"
+  description = "MongoDB Atlas private key"
 }
 
 variable "mongodbatlas_project_id" {}
