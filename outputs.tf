116354de2200a77d86afc5c5459f15d12fb79bef
Gus Narea <gnarea@users.noreply.github.com>
2023-07-20 12:30:38 +0100

    create topics

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,10 @@
 output "service_account_email" {
   value = google_service_account.endpoint.email
 }
+
+output "pubsub_topics" {
+  value = {
+    "incoming_messages" = google_pubsub_topic.incoming_messages.name
+    "outgoing_messages" = google_pubsub_topic.outgoing_messages.name
+  }
+}
