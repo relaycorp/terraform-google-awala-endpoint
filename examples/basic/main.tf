a62fe713331510ff26fe04d22993f6f0d5fe39a1
Gus Narea <gnarea@users.noreply.github.com>
2023-07-20 11:21:51 +0100

    create user

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,12 +1,13 @@
 locals {
   project_id = "tmp-tf-awala-endpoint"
+  gcp_region = "europe-west1"
 }
 
 module "self" {
   source = "../.."
 
   project_id = local.project_id
-  region     = "europe-west1"
+  region     = local.gcp_region
 
   depends_on = [google_project_service.services]
 }
--- examples/basic/mongodb.tf
+++ examples/basic/mongodb.tf
@@ -1,3 +1,7 @@
+locals {
+  mongodb_db_name = "main"
+}
+
 resource "mongodbatlas_serverless_instance" "main" {
   project_id = var.mongodbatlas_project_id
   name       = "awala-endpoint"
@@ -5,4 +9,40 @@ resource "mongodbatlas_serverless_instance" "main" {
   provider_settings_backing_provider_name = "GCP"
   provider_settings_provider_name         = "SERVERLESS"
   provider_settings_region_name           = "WESTERN_EUROPE"
+}
+
+resource "mongodbatlas_database_user" "main" {
+  project_id = var.mongodbatlas_project_id
+
+  username           = "awala-endpoint"
+  password           = random_password.mongodb_user_password.result
+  auth_database_name = "admin"
+
+  roles {
+    role_name     = "readWrite"
+    database_name = mongodbatlas_serverless_instance.main.name
+  }
+}
+
+resource "random_password" "mongodb_user_password" {
+  length = 32
+}
+
+resource "google_secret_manager_secret" "main" {
+  project = local.project_id
+
+  secret_id = "awala_endpoint-mongodb_password"
+
+  replication {
+    user_managed {
+      replicas {
+        location = local.gcp_region
+      }
+    }
+  }
+}
+
+resource "google_secret_manager_secret_version" "main" {
+  secret      = google_secret_manager_secret.main.id
+  secret_data = random_password.mongodb_user_password.result
 }
