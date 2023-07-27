d04877a40f4245a9282799141200626cc9ae9a2c
Gus Narea <gnarea@users.noreply.github.com>
2023-07-27 13:31:44 +0100

    fix: Add display name to service account

--- iam.tf
+++ iam.tf
@@ -1,5 +1,6 @@
 resource "google_service_account" "main" {
   project = var.project_id
 
-  account_id = "awala-endpoint-${random_id.resource_suffix.hex}"
+  account_id   = "awala-endpoint-${random_id.resource_suffix.hex}"
+  display_name = "Awala Internet Endpoint (${random_id.resource_suffix.hex})"
 }
